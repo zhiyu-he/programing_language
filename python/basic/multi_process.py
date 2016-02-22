ad55603b355adc742c2570c9e4426fcd339ae9ea
hezhiyu <hezhiyu@bytedance.com>
2016-02-23 00:17:59 +0800

    MOD: simulator consumer and producer
    Change-Id: Ib84d2933d9bc67fe9f7c1e679df8c98713db278e

--- python/basic/multi_process.py
+++ python/basic/multi_process.py
@@ -2,7 +2,18 @@
 
 import time
 import os
+import random
 from multiprocessing import Process, Queue
+
+def consumer(q):
+    # consumer 通过queue获取消费的数据
+    while True:
+        data = q.get()
+        if data is None:
+            print "nothing in the queue"
+            time.sleep(1)
+            continue
+        print "data: %s" % data
 
 def info(title):
     print title
@@ -17,7 +28,13 @@ def f(name):
         time.sleep(1)
 
 if __name__ == '__main__':
-    info('main line')
-    p = Process(target=f, args=('bob',))
+    q = Queue()
+    p = Process(target=consumer, args=(q,))
     p.start()
-    p.join()
+    # p.join()
+    print "can do"
+    while True:
+        num = random.randint(1, 100)
+        q.put(num)
+        sleep = random.randint(1, 5)
+        time.sleep(sleep)
